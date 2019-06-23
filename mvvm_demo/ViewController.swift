22e278d66ef78d4a951cc319a098aa7c428bdb98
Peter Basem <pero.bassem@gmail.com>
2019-06-23 13:05:33 +0200

    Initial Commit

---
+++ mvvm_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm_demo
+//
+//  Created by Peter Bassem on 6/23/19.
+//  Copyright © 2019 Peter Bassem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
